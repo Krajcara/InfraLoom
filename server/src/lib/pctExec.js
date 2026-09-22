7ecbd1535259a823c75077401994a6815199c07e
Krajcara <162749751+Krajcara@users.noreply.github.com>
2026-09-22 20:49:05 +0200

    1.1

--- server/src/lib/pctExec.js
+++ server/src/lib/pctExec.js
@@ -22,7 +22,7 @@ function execInLXC(conn, vmid, command, { timeoutMs = 300000, onOutput } = {}) {
     if (!/^\d+$/.test(String(vmid))) return reject(new Error(`Invalid LXC vmid: ${vmid}`));
 
     const shellEscaped = command.replace(/'/g, `'\\''`);
-    const fullCommand = `/usr/sbin/pct exec ${vmid} -- /bin/sh -c '${shellEscaped}'`;
+    const fullCommand = `export PATH="/usr/sbin:/usr/bin:/sbin:/bin:$PATH"; pct exec ${vmid} -- /bin/sh -c '${shellEscaped}'`;
 
     const client = new SSHClient();
     const timer = setTimeout(() => {
